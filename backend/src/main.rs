f7e69aa19a8028f1a156bae52ac769f3471748de
Juraj Paluba <juraj.palubajp@gmail.com>
2025-02-18 21:35:17 +0100

    feat(backend): create register runners endpoint
    Make the runners registrations more persistent, for example by using
    Redis.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,37 @@
-use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
+use actix_web::{
+    post,
+    web::{Data, Path},
+    App, HttpResponse, HttpServer, Responder,
+};
+
+#[post("/runners/{client_id}")]
+async fn echo(
+    req_body: String,
+    client_id: Path<String>,
+    runners: Data<Arc<RwLock<HashMap<String, String>>>>,
+) -> impl Responder {
+    runners
+        .write()
+        .unwrap()
+        .insert(client_id.into_inner(), "url".to_string());
+    println!("runners {:?}", runners.read().unwrap());
     HttpResponse::Ok().body(req_body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(echo))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let runners: Arc<RwLock<HashMap<String, String>>> = Arc::new(RwLock::new(HashMap::new()));
+    HttpServer::new(move || {
+        App::new()
+            .service(echo)
+            .app_data(Data::new(runners.clone()))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
